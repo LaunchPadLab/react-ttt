78ec3b45ef6dbefe07b63bab6532d5b55a75d524
Rachel Killackey <rachel-killackey@Rachels-MacBook-Pro-2.local>
2016-06-06 09:58:45 -0500

    win logic done

--- app/components/Game.js
+++ app/components/Game.js
@@ -20,16 +20,29 @@ export default class Game extends React.Component {
     }
   }
 
-  squareClick(position, player){
+  squareClick(position, turn){
     var board = this.state.board;
     if ( (board[position] === 'X' || board[position] === 'O') || (this.state.winner != undefined) ) return;
-    board[position] = player;
-    this.setState({board: board, turn: player === 'X' ? 'O' : 'X', winner: this.checkWinner()});
+    board[position] = turn;
+    this.setState({board: board, turn: turn === 'X' ? 'O' : 'X', winner: this.checkWinner()});
   }
 
   checkWinner() {
     var board = this.state.board;
-    //need to complete this logic
+    const checkEqual = (s1,s2,s3) => {
+      var concat = s1+s2+s3;
+      return concat === 'XXX' || concat === 'OOO';
+    }
+
+    if (checkEqual(board[0],board[1],board[2])) return board[0];
+    if (checkEqual(board[3],board[4],board[5])) return board[3];
+    if (checkEqual(board[6],board[7],board[8])) return board[6];
+    if (checkEqual(board[0],board[3],board[6])) return board[0];
+    if (checkEqual(board[1],board[4],board[7])) return board[1];
+    if (checkEqual(board[2],board[5],board[8])) return board[2];
+
+    if (board.join('').length == 9) return 'complete';
+    return undefined;
   }
 
   newGame() {
