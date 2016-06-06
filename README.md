6d2d0690443f798ef3654c538844bc64bfd42192
Rachel Killackey <rachel-killackey@Rachels-MacBook-Pro-2.local>
2016-06-06 10:10:18 -0500

    small updates

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # React Tic-Tac-Toe
 
-This is a Tic Tac Toe game built in React! To play, click the square you wish to place the 'X' or 'O' in. X's always go first.
+This is a Tic-Tac-Toe game built in React! To play, click the square you wish to place the 'X' or 'O' in. X's always go first.
 
 ## Getting started
 
+* `npm install`
 * Start server: `npm start`
--- app/components/GameInfo.js
+++ app/components/GameInfo.js
@@ -9,9 +9,7 @@ export default class GameInfo extends React.Component {
   }
 
   render() {
-    console.log(this.props.winner);
     return (
-
       <div id='gameInfo'>
         <h3 className={this.props.winner === undefined ? 'visible' : 'hidden'}>Player {this.props.turn}'s turn.</h3>
         <h3 className={(this.props.winner === undefined) || (this.props.winner === 'complete') ? 'hidden' : 'visible'}>Player {this.props.winner} won!</h3>
